fb78a17f480b9ed7723a8abbd2bb0e7559cce51b
Ivan <tred1000@mail.ru>
2023-01-19 23:14:16 +0300

    Created Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на чётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] GetRandomNumbersArray(int arrayLength = 4)
+{
+    int[] array = new int[arrayLength];
+    Random randomNumbers = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = randomNumbers.Next(-100, 100);
+    }
+    return array;
+}
+
+int GetSumEvenPositionNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i = i + 2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+int[] randomNumbersArray = GetRandomNumbersArray();
+int sumEvenPositionNumbers = GetSumEvenPositionNumbers(randomNumbersArray);
+Console.Write("Сумма элементов массива [" + string.Join(", ", randomNumbersArray) + "], стоящих на чётных позициях равна " + sumEvenPositionNumbers);
